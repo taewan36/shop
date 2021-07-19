38b4606fb3deeecbc2abf6637f37e8bdcde7b28b
vkrh0406 <vkrh0406@gmail.com>
2021-07-19 20:03:43 +0900

    Initial commit

---
+++ src/test/java/com/vkrh0406/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkrh0406.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
