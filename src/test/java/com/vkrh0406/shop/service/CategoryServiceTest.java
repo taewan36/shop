76c11f3a9b605600e3deeeba9d88ece9ea21286d
vkrh0406 <vkrh0406@gmail.com>
2021-07-31 01:03:20 +0900

    리눅스 게시를 위한 조정중

--- src/test/java/com/vkrh0406/shop/service/CategoryServiceTest.java
+++ src/test/java/com/vkrh0406/shop/service/CategoryServiceTest.java
@@ -55,30 +55,30 @@ class CategoryServiceTest {
     }
 
     private List<Category> createCategoryEntities() {
-        Category sub1 = new Category("SUB1"   ,1L, 0L);
-        Category sub2 = new Category("SUB2"   ,2L, 0L);
-        Category sub11 = new Category("SUB1-1",3L, 1L);
-        Category sub12 = new Category("SUB1-2",4L, 1L);
-        Category sub21 = new Category("SUB2-1",5L, 2L);
-        Category sub22 = new Category("SUB2-2",6L, 2L);
-
-        categoryRepository.save(sub1);
-        categoryRepository.save(sub2);
-        categoryRepository.save(sub11);
-        categoryRepository.save(sub12);
-        categoryRepository.save(sub21);
-        categoryRepository.save(sub22);
-
-//        ReflectionTestUtils.setField(sub1, "id", 1L);
-//        ReflectionTestUtils.setField(sub2, "id", 2L);
-//        ReflectionTestUtils.setField(sub11, "id", 3L);
-//        ReflectionTestUtils.setField(sub12, "id", 4L);
-//        ReflectionTestUtils.setField(sub21, "id", 5L);
-//        ReflectionTestUtils.setField(sub22, "id", 6L);
-
-        List<Category> categoryEntities = List.of(sub1, sub2, sub11, sub12, sub21, sub22);
-
-        return categoryEntities;
+//        Category sub1 = new Category("SUB1"   ,1L, 0L);
+//        Category sub2 = new Category("SUB2"   ,2L, 0L);
+//        Category sub11 = new Category("SUB1-1",3L, 1L);
+//        Category sub12 = new Category("SUB1-2",4L, 1L);
+//        Category sub21 = new Category("SUB2-1",5L, 2L);
+//        Category sub22 = new Category("SUB2-2",6L, 2L);
+//
+//        categoryRepository.save(sub1);
+//        categoryRepository.save(sub2);
+//        categoryRepository.save(sub11);
+//        categoryRepository.save(sub12);
+//        categoryRepository.save(sub21);
+//        categoryRepository.save(sub22);
+//
+////        ReflectionTestUtils.setField(sub1, "id", 1L);
+////        ReflectionTestUtils.setField(sub2, "id", 2L);
+////        ReflectionTestUtils.setField(sub11, "id", 3L);
+////        ReflectionTestUtils.setField(sub12, "id", 4L);
+////        ReflectionTestUtils.setField(sub21, "id", 5L);
+////        ReflectionTestUtils.setField(sub22, "id", 6L);
+//
+//        List<Category> categoryEntities = List.of(sub1, sub2, sub11, sub12, sub21, sub22);
+//
+//        return categoryEntities;
 
     }
 }
